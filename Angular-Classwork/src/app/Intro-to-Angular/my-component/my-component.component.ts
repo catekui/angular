dc7f2ee12f04b7ed3bf4d2d7725ac3faaf2fede8
catekui <catherine.wanjiku@student.moringaschool.com>
2022-04-09 00:43:31 +0300

    add bootstrap

--- Angular-Classwork/src/app/Intro-to-Angular/my-component/my-component.component.ts
+++ Angular-Classwork/src/app/Intro-to-Angular/my-component/my-component.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html',
+  // template: 'This is my Component'
   styleUrls: ['./my-component.component.css']
 })
 export class MyComponentComponent implements OnInit {
